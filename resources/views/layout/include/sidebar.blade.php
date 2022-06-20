279f109f8d5fcbc1a9f6006e5a65ff05d0e104e6
izarAhmad <izardomains@gmail.com>
2022-06-20 13:45:57 +0700

    update 2

--- resources/views/layout/include/sidebar.blade.php
+++ resources/views/layout/include/sidebar.blade.php
@@ -1,15 +1,1 @@
 
-            {{--<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-            <img src="
-                            {{auth()->user()->profile->getAvatar()}}
-            " class="img-circle elevation-2" alt="User Image">
-            </div>
-            <div class="info">
-            <a href="#" class="d-block">{{Auth()->user()->profile->nama_lengkap}}</a>
-            </div>
-			</ul>--}}
-           
-
-<!-- navbar -->
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -16,7 +16,18 @@
     @yield('header')
     </head>
     <!-- Sidebar -->
-    @include('layout.include.sidebar')
+    
+    {{--<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+            <img src="
+                            {{auth()->user()->profile->getAvatar()}}
+            " class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+            <a href="#" class="d-block">{{Auth()->user()->profile->nama_lengkap}}</a>
+            </div>
+			</ul>--}}
         <!-- /.sidebar -->
         <section id="content">
     <!-- Navbar -->
