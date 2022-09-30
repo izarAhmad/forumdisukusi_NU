ca2f2db1c5d33569db2c28beac5409d38d389818
izarAhmad <izardomains@gmail.com>
2022-09-30 19:14:19 +0700

    update header

--- resources/views/layout/home.blade.php
+++ resources/views/layout/home.blade.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Forum Diskusi</title>
+  <meta name="dicoding:email" content="roudlotunnizar56@gmail.com">
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
